e2cd01fb56dfd0395da684207f4b1589094fd63c
Dexter Florendo <imdex.r.florendo@gmail.com>
2017-11-02 14:09:49 -0700

    11

--- handlers/registration-list.php
+++ handlers/registration-list.php
@@ -13,10 +13,11 @@ $con = new pdo_db();
 $personal_infos = $con->getData("SELECT * FROM personal_infos");
 
  // Convert id to array
-foreach ($personal_infos as $key => $personal_info) {
+foreach($personal_infos as $key => $personal_info){
 	
 	$barangay = $con->getData("SELECT id, barangay_description FROM barangays WHERE barangays.id = ".$personal_info['address_barangay']);
 	$personal_infos[$key]['address_barangay'] = $barangay[0];
+	
 	$municipalities = $con->getData("SELECT id, municipality FROM municipalities WHERE municipalities.id = ".$personal_info['address_municipality']);
 	$personal_infos[$key]['address_municipality'] = $municipalities[0];
 }
--- handlers/registration-view.php
+++ handlers/registration-view.php
@@ -10,7 +10,11 @@ $con = new pdo_db();
 
 $personal_infos = $con->getData("SELECT * FROM personal_infos WHERE id = $_POST[id]");
 
+
+
 foreach($personal_infos as $key => $personal_info){
+	
+$personal_infos[$key]['family_head'] = ($personal_infos[$key]['address_barangay'])?true:false; 
 	
 	$barangay = $con->getData("SELECT id, barangay_description FROM barangays WHERE barangays.id = ".$personal_info['address_barangay']);
 	$personal_infos[$key]['address_barangay'] = $barangay[0];
