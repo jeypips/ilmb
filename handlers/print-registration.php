1805a97e29aeb015d5f099d1ac5a18430163f698
jeypips <johnpaulbalanon6@gmail.com>
2017-11-03 11:47:46 +0800

    n

--- handlers/print-registration.php
+++ handlers/print-registration.php
@@ -4,19 +4,18 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 
 require_once '../db.php';
 
-require_once '../classes.php';
-
 $con = new pdo_db();
-
-$registrationObj = new registration($con);
 
 $id = $_POST['id'];
 
-$personal_infos = $con->getData("SELECT * FROM personal_infos");
+$personal_info = $con->getData("SELECT *, DATE_FORMAT(birth_date, '%M %l, %Y') birth_date, IF(family_head=1,'Yes','No') family_head, IF(employment_status='true','Yes','No') employment_status, IF(philhealth_member='true','Yes','No') philhealth_member FROM personal_infos where id= $_POST[id]");
+// $personal_info = $con->getData("SELECT * FROM personal_infos where id= $_POST[id]");
 
-$personal_infos = [];
+// $personal_info[0]['birth_date'] = date("F j, Y", strtotime($personal_info[0]['birth_date']));
+// $personal_info[0]['family_head'] = ($personal_info[0]['family_head'])?"Yes":"No";
+// $personal_info[0]['employment_status'] = ($personal_info[0]['employment_status'] == "true")?"Yes":"No";
 
 header("Content-Type: application/json");
-echo json_encode($personal_infos);
+echo json_encode($personal_info[0]);
 
 ?>
