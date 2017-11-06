fa5d880c568a61e3373dfc6dc99b2cd2b28cdcca
Sly Flores <sly@christian.com.ph>
2017-11-06 16:17:49 +0800

    activate event

--- handlers/event-list.php
+++ handlers/event-list.php
@@ -16,6 +16,7 @@ foreach($events as $key => $event){
 	
 	$municipalities = $con->getData("SELECT id, municipality FROM municipalities WHERE municipalities.id = ".$event['town']);
 	$events[$key]['town'] = $municipalities[0];
+	$events[$key]['is_active'] = ($events[$key]['is_active'])?true:false;
 }
 
 header("Content-Type: application/json");
