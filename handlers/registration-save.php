2aaaf1bafa43c15b57f864f220c5a7e652f55d94
jeypips <johnpaulbalanon6@gmail.com>
2017-11-06 09:27:49 +0800

    ..

--- handlers/registration-save.php
+++ handlers/registration-save.php
@@ -10,13 +10,17 @@ $_POST['personal_info']['family_head'] = ($_POST['personal_info']['family_head']
 $_POST['personal_info']['address_barangay'] = $_POST['personal_info']['address_barangay']['id'];
 $_POST['personal_info']['address_municipality'] = $_POST['personal_info']['address_municipality']['id'];
 
+$_POST['personal_info']['family_members'] = (!$_POST['personal_info']['family_head'])?0:$_POST['personal_info']['family_members']; // checkbox 0 value
+
 if ($_POST['personal_info']['id']) {
 	
 	$personal_info = $con->updateData($_POST['personal_info'],'id');
 	
+
 } else {
 	
 	$personal_info = $con->insertData($_POST['personal_info']);
+	
 	echo $con->insertId;
 
 }
