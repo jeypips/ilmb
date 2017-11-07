4f2710a19dff4e3dd74b2cfa1fac73f454e2ecd3
Sly Flores <sly@christian.com.ph>
2017-11-07 16:59:14 +0800

    ...

--- api/services/profile.php
+++ api/services/profile.php
@@ -12,6 +12,16 @@ $con = new pdo_db("services_availed");
 $services = $con->getData("SELECT * FROM services");
 
 $profile = $con->getData("SELECT personal_info_no, CONCAT(lastname, ', ', firstname, ' ', middlename) fullname, DATE_FORMAT(birth_date,'%M %e, %Y') birth_date, age, (SELECT barangay_description FROM barangays WHERE id = address_barangay) barangay FROM personal_infos WHERE id = ".$_POST['id']);
+$profile[0]['services'] = [];
+
+foreach ($services as $service) {
+	
+	if (!qualifiedService($profile[0]['age'],$service)) continue;	
+	$profile[0]['services'][] = array(
+	
+	);
+	
+};
 
 header("Content-Type: application/json");
 echo json_encode($profile[0]);
@@ -20,7 +30,7 @@ function qualifiedService($age,$service) {
 	
 	$qualifiedService = false;
 	
-	
+	if ($age <= $service['max_age']) $qualifiedService = true; 
 	
 	return $qualifiedService;
 	
