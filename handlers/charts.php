b286afa4f877153cc00d27ed0d1ba344a20d7144
jeypips <johnpaulbalanon6@gmail.com>
2017-11-09 13:07:21 +0800

    ..

---
+++ handlers/charts.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+require_once '../db.php';
+
+session_start();
+
+$con = new pdo_db();
+
+$personal_infos = $con->getData("SELECT * FROM personal_infos");
+
+header("Content-Type: application/json");
+echo json_encode($personal_infos);
+
+?>
